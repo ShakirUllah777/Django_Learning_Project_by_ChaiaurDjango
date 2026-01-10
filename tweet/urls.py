20725bd5931c8e7cc2093b5288bfb7f912db9f16
turkish777 <shakir.turk777@gmail.com>
2026-01-10 12:34:46 +0500

    full end to end django project

---
+++ tweet/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.conf import settings
+from . import views
+
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('', views.tweet_list, name='tweet_list'),
+    path('create/', views.tweet_create, name='tweet_create'),
+    path('edit/<int:tweet_id>/', views.tweet_edit, name='tweet_edit'),
+    path('delete/<int:tweet_id>/', views.tweet_delete, name='tweet_delete'),
+    path('register/', views.register, name='register'),
+]
